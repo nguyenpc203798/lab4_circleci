3eb2178f424c35be9111a51851afd38a6078e18e
nguyenpc203798 <npdz321@gmail.com>
2024-12-26 23:10:43 +0700

    update file main

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ func main() {
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
-			"message": "pong 123",
+			"message": "Nguyen dep trai",
 		})
 	})
 	r.Run(":" + PORT)
